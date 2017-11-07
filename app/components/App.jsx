72fb2288671a980fff794e8dd07bf64a1e0fdb3a
Manuel Ramos Gonzalez <maramos0216@gmail.com>
2017-11-07 23:36:15 +0000

    Added working delete button

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,26 +1,7 @@
 import React from 'react';
 import Notes from './Notes';
 import uuid from 'uuid';
-/*
-const notes = [
-    
-    {
-        //id: '4e81fc6e-bfb6-419b-93e5-0242fb6f3f6a',
-        id: uuid.v4(),
-        task: 'Learn React'
-        
-    },
-    
-    
-    {
-        //id: '11bbffc8-5891-4b45-b9ea-5c99aadf870f',
-        id: uuid.v4(),
-        task: 'Go to code the dream'
-    },
-    
-    
-    ];
-*/
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -43,19 +24,33 @@ export default class App extends React.Component {
 
     return (
       <div>
-        <button onClick={() => console.log('add note')}>+</button>
-        <Notes notes={notes} />
+        <button onClick={this.addNote}>+</button>
+        
+        <Notes notes={notes} onDelete = {this.deleteNote} />
       </div>
     );
   }
-}
-/*
-export default () => (
-    <div>
-        <button onClick={() => console.log('add note')}>+</button>
+
+
+   addNote = () => {
         
+        this.setState({
+            
+            notes: this.state.notes.concat([{
+        id: uuid.v4(),
+        task: 'New task'
+                
+            }])
+        });
+    }
     
-        <Notes notes={notes}/>
-    </div>
-    
-    );*/
+    deleteNote = (id,e) => {
+        e.stopPropagation();
+        
+        this.setState({
+          
+            notes: this.state.notes.filter(note => note.id !== id)
+        });
+      
+    }
+}
--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -1,29 +1,21 @@
 import React from 'react';
+import Note from './Note';
 
-/*import uuid from 'uuid';
-
-const notes = [
-    
-    {
-        //id: '4e81fc6e-bfb6-419b-93e5-0242fb6f3f6a',
-        id: uuid.v4(),
-        task: 'Learn React'
-        
-    },
-    
-    
-    {
-        //id: '11bbffc8-5891-4b45-b9ea-5c99aadf870f',
-        id: uuid.v4(),
-        task: 'Go to code the dream'
-    },
-    
-    
-    ]; */
-    
+/*    
 export default ({notes}) => (
     <ul>{notes.map(note=>
-        <li key={note.id}>{note.task}</li>
+        <li key={note.id}><Note task = {note.task}/></li>
     )}
     </ul>
+    )
+*/    
+export default ({notes, onDelete = () =>{}}) => (
+    <ul>{notes.map(({id, task}) =>
+        <li key={id}>
+            <Note
+                onDelete = {onDelete.bind(null,id)}
+                task = {task}
+            />
+          </li>
+        )}</ul>
     )
