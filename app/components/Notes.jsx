8d686de43d2de9f3460323984397fd3c90d10359
Manuel Ramos Gonzalez <maramos0216@gmail.com>
2017-11-14 05:17:38 +0000

    Added editing logic

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Note from './Note';
+import Editable from './Editable';
 
 /*    
 export default ({notes}) => (
@@ -9,11 +10,15 @@ export default ({notes}) => (
     </ul>
     )
 */    
-export default ({notes, onDelete = () =>{}}) => (
-    <ul>{notes.map(({id, task}) =>
+export default ({notes, onNoteClick=() => {}, onEdit = () => {}, onDelete = () =>{}}) => (
+    <ul>{notes.map(({id, editing, task}) =>
         <li key={id}>
-            <Note>
-                <span>{task}</span>
+            <Note onClick = {onNoteClick.bind(null, id)} >
+                <Editable
+                editing = {editing}
+                value = {task}
+                onEdit = {onEdit.bind(null, id)}
+                />
                 <button onClick={onDelete.bind(null,id)}>x</button>
             </Note>
           </li>
