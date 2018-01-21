0c7dbb7459fe1ab9d212271af4152a2a75ab0e7f
Manuel Ramos Gonzalez <maramos0216@gmail.com>
2018-01-21 19:21:58 +0000

    completed implementing flux arch

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -62,16 +62,18 @@ class App extends React.Component {
     
     deleteNote = (id,e) => {
         e.stopPropagation();
-        
+        /*
         this.setState({
           
             notes: this.state.notes.filter(note => note.id !== id)
-        });
+        });*/
+      
+      this.props.NoteActions.delete(id);
       
     }
     
     activateNoteEdit = (id) => {
-      this.setState({
+      /*this.setState({
         notes: this.state.notes.map(note =>{
           if (note.id === id) {
             note.editing = true;
@@ -79,12 +81,13 @@ class App extends React.Component {
           return note;
         })
         
-      });
+      });*/
+      this.props.NoteActions.update({id, editing: true});
       
     }
     
     editNote = (id, task) => {
-      this.setState({
+      /*this.setState({
         notes: this.state.notes.map(note => {
           if (note.id === id) {
             note.editing = false;
@@ -92,7 +95,8 @@ class App extends React.Component {
           }
           return note;
         })
-      });
+      });*/
+      this.props.NoteActions.update({id,task,editing: false});
     }
 
 }
--- app/stores/NoteStore.js
+++ app/stores/NoteStore.js
@@ -17,13 +17,26 @@ export default class NoteStore {
   }
   
   create(note){
-    console.log('create note', note);
+    this.setState({
+      notes: this.notes.concat(note)
+    });
   }
   update(updatedNote){
-    console.log('update Note', updatedNote);
+    this.setState({
+      notes: this.notes.map( note =>{
+        if (note.id === updatedNote.id){
+          return Object.assign({},note, updatedNote);
+        }
+        
+          return note;
+        
+      })
+    });
   }
   delete(id){
-    console.log('delete note', id);
+    this.setState({
+      notes: this.notes.filter(note => note.id != id)
+    });
   }
   
 }
