149cad873e14a4635168906080a507f353ada607
Manuel Ramos Gonzalez <maramos0216@gmail.com>
2018-01-24 05:59:18 +0000

    added lane header component

--- app/components/Lane.jsx
+++ app/components/Lane.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import uuid from 'uuid';
 import connect from '../libs/connect';
 import NoteActions from '../actions/NoteActions';
 import LaneActions from '../actions/LaneActions';
 import Notes from './Notes';
+import LaneHeader from './LaneHeader'
 
 const Lane = ({
   lane, notes, NoteActions, ...props, LaneActions
@@ -11,7 +11,7 @@ const Lane = ({
   const editNote = (id, task) => {
     NoteActions.update({id, task, editing: false});
   };
-  const addNote = e => {
+  /*const addNote = e => {
     e.stopPropagation();
 
     const noteId = uuid.v4();
@@ -25,7 +25,7 @@ const Lane = ({
       laneId: lane.id,
       noteId
     });
-  };
+  };*/
   const deleteNote = (noteId, e) => {
     e.stopPropagation();
     
@@ -42,12 +42,7 @@ const Lane = ({
 
   return (
     <div {...props}>
-      <div className="lane-header">
-        <div className="lane-add-note">
-          <button onClick={addNote}>+</button>
-        </div>
-        <div className="lane-name">{lane.name}</div>
-      </div>
+      <LaneHeader lane = {lane}/>
       <Notes
         notes={selectNotesByIds(notes,lane.notes)}
         onNoteClick={activateNoteEdit}
